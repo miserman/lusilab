f96a52b2da6b85585e3762fe65ff83794a4366f6
Micah <micah.iserman@gmail.com>
2022-08-01 09:39:14 -0400

    adds sparse support

--- R/lusilab-package.R
+++ R/lusilab-package.R
@@ -1,10 +1,11 @@
 #' @importFrom utils write.csv download.file read.csv unzip
 #' @importFrom RedditExtractoR find_thread_urls get_thread_content
 #' @importFrom jsonlite fromJSON
-#' @importFrom lingmatch lingmatch lma_termcat lma_weight lma_dtm
+#' @importFrom lingmatch lingmatch lma_termcat lma_weight lma_dtm lma_simets
 #' @importFrom httr GET content
 #' @importFrom parallel detectCores makeCluster clusterExport stopCluster parLapply
 #' @importFrom stats cor pf pnorm qf qnorm quantile uniroot
+#' @import Matrix
 #' @keywords internal
 "_PACKAGE"
 
