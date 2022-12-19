d04193862889eb039708121d2cec5bf2a152a770
Micah <micah.iserman@gmail.com>
2022-12-19 15:25:56 -0500

    adds babelnet_to_wordnet

--- R/lusilab-package.R
+++ R/lusilab-package.R
@@ -1,11 +1,11 @@
 #' @importFrom utils write.csv download.file read.csv unzip
 #' @importFrom RedditExtractoR find_thread_urls get_thread_content
-#' @importFrom jsonlite fromJSON read_json
+#' @importFrom jsonlite fromJSON read_json write_json
 #' @importFrom lingmatch lingmatch lma_termcat lma_weight lma_dtm lma_simets
 #' @importFrom httr GET content write_disk
 #' @importFrom parallel detectCores makeCluster clusterExport stopCluster parLapply
 #' @importFrom stats cor pf pnorm qf qnorm quantile uniroot
-#' @importFrom arrow read_csv_arrow write_csv_arrow write_dataset open_dataset
+#' @importFrom arrow read_csv_arrow read_delim_arrow write_csv_arrow write_dataset open_dataset
 #' @importFrom dplyr filter collect summarise group_by n
 #' @importFrom nlme lme
 #' @importFrom stats na.omit as.formula lm fitted resid
